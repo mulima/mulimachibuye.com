18ac19cdb9933e01412d1dbeb436a98f1f153304
mulima <mchibuye@gmail.com>
2025-07-07 10:27:52 +0200

    update ecosystem.config.js, added npx

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,30 @@
 module.exports = {
-  apps: [{
-    name: 'mulimachibuye.com',
-    script: 'npm',
-    args: 'start',
-    cwd: '/var/www/mulimachibuye.com',
-    instances: 1,
-    exec_mode: 'fork',
-    env: {
-      NODE_ENV: 'development',
-      PORT: 3000
+  apps: [
+    {
+      name: "mulimachibuye.com",
+      script: "npx",
+      args: "serve@latest out",
+      cwd: "/var/www/mulimachibuye.com",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+      error_file: "/var/www/mulimachibuye.com/logs/err.log",
+      out_file: "/var/www/mulimachibuye.com/logs/out.log",
+      log_file: "/var/www/mulimachibuye.com/logs/combined.log",
+      time: true,
+      max_memory_restart: "1G",
+      restart_delay: 4000,
+      watch: false,
+      ignore_watch: ["node_modules", "logs", ".next"],
+      autorestart: true,
+      kill_timeout: 5000,
     },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 3000
-    },
-    error_file: '/var/www/mulimachibuye.com/logs/err.log',
-    out_file: '/var/www/mulimachibuye.com/logs/out.log',
-    log_file: '/var/www/mulimachibuye.com/logs/combined.log',
-    time: true,
-    max_memory_restart: '1G',
-    restart_delay: 4000,
-    watch: false,
-    ignore_watch: ['node_modules', 'logs', '.next'],
-    autorestart: true,
-    kill_timeout: 5000
-  }]
+  ],
 };
